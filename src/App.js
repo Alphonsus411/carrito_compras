868a1b47b8eae8adf1e6766fcb43045a89bae8aa
Adolfo Gonzalez <adolfogonzal@gmail.com>
2026-08-27 10:46:20 +0200

    Cambios en App.js y creación de carpeta /productos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    productos: [
+      { name: 'Tomate', price: 2, img:'/productos/tomate.jpg'},
+      { name: 'Arbejas', price: 6, img:'/productos/arbejas.jpg'},
+      { name: 'Lechuga', price: 2, img:'/productos/lechuga.jpg'}
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <p>Hola mundo</p>
+      </div>
+    )
+  }
 }
-
+  
+  
+  
+  
 export default App;
