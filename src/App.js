10ca8530bba69a7d884bf4d051c839e3ef213d3b
Adolfo Gonzalez <adolfogonzal@gmail.com>
2026-08-27 13:41:42 +0200

    Cambios en App.js y creación de Layout.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import Productos from './components/Productos.js'
+import Layout from './components/Layout'
 
 class App extends Component {
   state = {
@@ -12,11 +13,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Productos
+        <Layout>
+          <Productos
           agregarAlCarro={() => console.log("Por favor, espere...")}
           productos={this.state.productos}
         />
         
+        </Layout>
       </div>
     )
   }
