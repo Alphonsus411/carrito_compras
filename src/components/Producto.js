fe0f3b09501ddc7fbd8e686b5d83223ab9a793b7
Adolfo Gonzalez <adolfogonzal@gmail.com>
2026-08-27 11:52:44 +0200

    Cambios en Producto.js para añadir estilos a los productos

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,10 +1,18 @@
 import { Component } from "react";
 
+const styles = {
+   poducto: {
+    border: 'solid 1px #eee',
+    boxShadow: '0 5px 5px rgb(0, 0, 0, 0.1)',
+
+   } 
+}
+
 class Producto extends Component {
     render() {
         const { producto } = this.props
         return (
-            <div>
+            <div styles={styles.producto}>
                 <img alt={producto.name} src={producto.img} />
                 <h3>{producto.name}</h3>
                 <p>{producto.price}</p>
