36e5eec5f3f265ab59f07ef5db2c02ff31f721a5
Adolfo Gonzalez <adolfogonzal@gmail.com>
2026-08-28 09:28:04 +0200

    Cambios en App.js

--- src/App.js
+++ src/App.js
@@ -9,17 +9,42 @@ class App extends Component {
     productos: [
       { name: 'Tomate', price: 2, img:'/productos/tomate.jpg'},
       { name: 'Arbejas', price: 6, img:'/productos/arbejas.jpg'},
-      { name: 'Lechuga', price: 2, img:'/productos/lechuga.jpg'}
-    ]
+      { name: 'Lechuga', price: 2, img:'/productos/lechuga.jpg'},
+    ],
+    carro: [
+      //{ name: 'Tomate', price: 2, img:'/productos/tomate.jpg', cantidad: 1 },  
+    ],
   }
+
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state
+    if (carro.find(x => x.name === producto.name)) {
+      const newCarro = carro.map(x=> x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1
+        })
+      : x)
+      return this.setState({ carro: newCarro })
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    })
+
+  }
+
   render() {
+    console.log(this.state.carro)
     return (
       <div>
         <Navbar />
         <Layout>
           <Title />
           <Productos
-          agregarAlCarro={() => console.log("Por favor, espere...")}
+          agregarAlCarro={this.agregarAlCarro}
           productos={this.state.productos}
         />
         
